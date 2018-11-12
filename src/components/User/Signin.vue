f5fdbeb7f9bce09f6e2a4db992375a22cf7b4f25
vyshnavsdeepak <vyshnav@11thwizard.com>
2018-11-12 07:33:36 +0530

    Signin page directs to profile page on success

--- src/components/User/Signin.vue
+++ src/components/User/Signin.vue
@@ -77,7 +77,7 @@ export default {
   watch: {
     user (value) {
       if (value !== null && value !== undefined) {
-        this.$router.push('/')
+        this.$router.push('/profile')
       }
     }
   },
